eb7c8520cdd81191c9b38eee1f94c25aeecd79ef
Luca Delucchi <lucadeluge@gmail.com>
2023-06-10 00:24:42 +0200

    using UserAdmin

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 from euromammals.functions_admin import CSVAdmin
@@ -8,7 +9,7 @@ from .models import Organization
 from .models import ResearchGroup
 from .models import User
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Project, CSVAdmin)
 admin.site.register(Organization, CSVAdmin)
 admin.site.register(ResearchGroup, CSVAdmin)
