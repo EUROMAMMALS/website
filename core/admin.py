bf2ab4671725ba4b6102e6e0d34da2e67a83aeee
Luca Delucchi <lucadeluge@gmail.com>
2023-06-12 11:24:38 +0200

    added function to upload user from csv

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,18 @@
+import os
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib import messages
+from django.urls import path
+from django.shortcuts import redirect
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseNotFound
+from django.conf import settings
 
 # Register your models here.
 from euromammals.functions_admin import CSVAdmin
+from euromammals.functions_admin import CsvImportForm
+from euromammals.functions_admin import csv_exists
 
 from .models import Project
 from .models import Organization
@@ -33,6 +43,94 @@ class CustomUserAdmin(UserAdmin):
     )
     search_fields = ("email",)
     ordering = ("last_name", "first_name",)
+
+    change_list_template = "admin/import_csv.html"
+
+    def _add_urls(self):
+        return []
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path("import-csv/", self.import_csv),
+            path("download-csv/", self.get_csv),
+        ]
+        my_urls.extend(self._add_urls())
+        return my_urls + urls
+
+    def import_csv(self, request):
+        if request.method == "POST":
+            import django.contrib.auth
+            usermodel = django.contrib.auth.get_user_model()
+            csv_file = request.FILES["file"]
+            separator = request.POST.get("separator")
+            lines = csv_file.readlines()
+            header = lines[0].split(separator)
+            for line in lines[1:]:
+                vals = line.split(separator)
+                rg = vals[header.index("research_group")]
+                username = vals[header.index("username")]
+                try:
+                    regroup = ResearchGroup.objects.get(id=rg)
+                except:
+                    try:
+                        regroup = ResearchGroup.objects.get(name=rg)
+                    except:
+                        try:
+                            regroup = ResearchGroup.objects.get(shortname=rg)
+                        except:
+                            self.message_user(
+                                request,
+                                f"Research group with value {rg} not found. User {username} not upload",
+                                level=messages.ERROR
+                            )
+                user = usermodel.objects.create_user(
+                    email = vals[header.index("email")],
+                    username = username,
+                    password = vals[header.index("password")],
+                    is_staff = False,
+                    first_name = vals[header.index("first_name")],
+                    last_name = vals[header.index("last_name")],
+                    bio = vals[header.index("bio")],
+                    research_group = regroup
+                )
+                for projval in vals[header.index("projects")].split(","):
+                    try:
+                        proj = Project.objects.get(name=projval)
+                    except:
+                        try:
+                            proj = Project.objects.get(id=projval)
+                        except:
+                            self.message_user(
+                                request,
+                                f"Project {rg} not found and not set to user {username}",
+                                level=messages.ERROR
+                            )
+                    user.projects.add()
+                user.save()
+            self.message_user(request, "Your csv file has been importeds")
+            return redirect("..")
+        form = CsvImportForm()
+        payload = {"form": form}
+        return render(request, "admin/csv_form.html", payload)
+
+    def get_csv(self, request):
+        table = self.model._meta.db_table
+        csv = csv_exists(table)
+        csvpath = None
+        for inpath in settings.STATICFILES_DIRS:
+            tmpcsvpath = os.path.join(inpath, csv.lstrip("/"))
+            if os.path.exists(tmpcsvpath):
+                csvpath = tmpcsvpath
+                break
+        if not csvpath:
+            return HttpResponseNotFound(f"CSV file for model {self.model} not found")
+        with open(csvpath) as csvfile:
+            data = csvfile.read()
+        output = HttpResponse(data, content_type='text/csv')
+        output['Content-Disposition'] = f'attachment; filename={table}.csv'
+        return output
+
 
 admin.site.register(User, CustomUserAdmin)
 admin.site.register(Project, CSVAdmin)
