893a5f0cf7449936998c70dbf8de7a8fc3dd06f8
Luca Delucchi <lucadeluge@gmail.com>
2023-06-12 09:11:50 +0200

    improved registration form

--- core/admin.py
+++ core/admin.py
@@ -9,7 +9,32 @@ from .models import Organization
 from .models import ResearchGroup
 from .models import User
 
-admin.site.register(User, UserAdmin)
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    fieldsets = (
+        (None, {"fields": ("email", "password", "first_name", "last_name")}),
+        ("Personal info", {"fields": ["bio", "research_group", "projects", "image"]}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active", "is_superuser", "groups", "user_permissions",
+                "bio", "research_group", "projects", "image"
+            )}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("last_name", "first_name",)
+
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Project, CSVAdmin)
 admin.site.register(Organization, CSVAdmin)
 admin.site.register(ResearchGroup, CSVAdmin)
