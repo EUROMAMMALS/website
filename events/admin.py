608ed6c23e8e9b88ee187cf7c3198b9e24bb003b
Luca Delucchi <lucadeluge@gmail.com>
2024-03-05 10:08:49 +0100

    removed foreignkey to project keep the manytomany one

--- events/admin.py
+++ events/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
 from euromammals.functions_admin import CSVAdmin
-from euromammals.functions_admin import FilterProjectAdmin
 
 from .models import Event
 from .models import EventExternal
 
-class EventAdmin(CSVAdmin, FilterProjectAdmin):
+class EventAdmin(CSVAdmin):
     model = Event
+    list_filter = ["projects"]
 
 admin.site.register(Event, EventAdmin)
 admin.site.register(EventExternal, CSVAdmin)
--- events/models.py
+++ events/models.py
@@ -23,7 +23,6 @@ class Event(models.Model):
     typ = models.CharField(max_length=2, choices=EVENT_TYPE,
                            blank=True, null=True)
     venue = models.CharField(max_length=255, null=True, blank=True)
-    project = models.ForeignKey(Project, on_delete=models.PROTECT, related_name='project')
     projects = models.ManyToManyField(Project, related_name='projects')
     as_participant = models.BooleanField(null=True, blank=True)
     # privato
